e639b177b6bf9f93724467ba0082c39cd8bf693d
Natalia <tsepi2014@hotmail.com>
2023-02-25 13:13:10 +0100

    lesson_2 Added templates and Bootstrap styles

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from blog.views.users import users_app
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello!"
+    return render_template("index.html")
+
+
+app.register_blueprint(users_app, url_prefix="/users")
