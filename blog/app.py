eafc49c36e4f47ec30b8304eca2316291b02fbd0
Natalia <tsepi2014@hotmail.com>
2023-02-26 21:34:57 +0100

    lesson_3 Created user and article models, connected database

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+from blog.models.database import db
 from blog.views.articles import articles_app
 from blog.views.users import users_app
 
@@ -13,3 +14,7 @@ def index():
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from blog.app import app
+from blog.models.database import db
 
 if __name__ == "__main__":
     app.run(
@@ -6,3 +7,30 @@ if __name__ == "__main__":
         debug=True,
         port=5010,
     )
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in your terminal:
+    flask init-db
+    """
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in your terminal:
+    flask create-users
+    > done! created users: <User #1 'admin'> <User #2 'james'>
+    """
+    from blog.models import User
+
+    admin = User(username="admin", is_staff=True)
+    james = User(username="james")
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+    print("done! created users:", admin, james)
