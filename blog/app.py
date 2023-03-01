695a6e7366b9b6154391bcbd137b153776e4c8ab
Natalia <tsepi2014@hotmail.com>
2023-03-01 10:42:56 +0100

    lesson_4 docker

--- blog/app.py
+++ blog/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 from blog.models.database import db
@@ -17,9 +19,13 @@ app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
 app.register_blueprint(auth_app, url_prefix="/auth")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = "abcdefg123456"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.config["SECRET_KEY"] = "abcdefg123456"
+
+cfg_name = os.environ.get("CONFIG_NAME") or "ProductionConfig"
+app.config.from_object(f"blog.configs.{cfg_name}")
+
 db.init_app(app)
 
 login_manager.init_app(app)
